defea2ace775905d595b95e3a069d787ae4160c9
João Victor Batista de Oliveira <jvboliveira0405@gmail.com>
2025-09-01 14:56:05 -0300

    Trabalhando com operadores aritméticos

--- get-started/src/Main.java
+++ get-started/src/Main.java
@@ -5,28 +5,24 @@ public class Main {
 	public static void main(String[] args) {	
 
 		var scanner = new Scanner(System.in);
-		/*
+
+		System.out.println("Informe o primeiro número: ");
+		var value1 = scanner.nextInt(); //No caso de número racionais, usar nextfloat
+		System.out.println("Informe o segundo número: ");
+		var value2 = scanner.nextInt();
 		
-		System.out.printf("Quanto é 2 + 2 ?\n");
-		var result = scanner.nextInt();
-		var isRight = result == 4; Operador de igualdade
-		System.out.printf("O resultado é 4, você acertou? (%s)", isRight);
-		var isWrong = result != 4; //Operador de desigualdade
-		System.out.printf("O resultado é 4, você errou? (%s)\n", isWrong);
-		System.out.printf("O resultado é 4, você acertou? (%s)\n", !isWrong); //Operador de inversão
-		
-		*/
-		
-		System.out.println("Quantos anos você tem? ");
-		var age = scanner.nextInt();
-		System.out.println("Você é emancipado? ");
-		var isEmancipated = scanner.nextBoolean();
-		//var canDrive = age > 17; //Operador de comparação "Maior". Mesmo para "Menor"
-		var canDrive = age >= 18 || isEmancipated; //Operador de comparação "Maior ou igual". Mesmo para "Menor ou igual"
-		System.out.printf("Você pode dirigir? (%s)", canDrive);
+		System.out.printf("%s + %s = %s\n", value1, value2, value1+value2);
+		System.out.printf("%s - %s = %s\n", value1, value2, value1-value2);
+		//System.out.printf("%s / %s = %.2f\n", value1, value2, value1/value2);
+		System.out.printf("%s %% %s = %s\n", value1, value2, value1%value2);
+		System.out.printf("%s * %s = %s\n", value1, value2, value1*value2);
 		
 		scanner.close();
 		
+		var value = 50;
+		System.out.println(++value);
+		System.out.println(10 + value++);		
+		System.out.println(10 + value);
 	}
 
 }
