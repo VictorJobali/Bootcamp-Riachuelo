364939f8bbcc7af181f92740f8a0456d0a549a62
João Victor Batista de Oliveira <jvboliveira0405@gmail.com>
2025-09-01 14:20:21 -0300

    Trabalhando com operadores de atribuições e lógicos

--- get-started/src/Main.java
+++ get-started/src/Main.java
@@ -1,19 +1,31 @@
 import java.util.Scanner;
 
 public class Main {
-
-	private final static String WELCOME_MESSAGE = "Olá, informe o seu nome: ";
 	
 	public static void main(String[] args) {	
-		Scanner scan = new Scanner(System.in);
-		System.out.println(WELCOME_MESSAGE);
-		String name = scan.next();
-		System.out.println("informe sua idade: ");
-		int age = scan.nextInt();
-		
-		System.out.printf("Olá %s sua idade é %d anos \n", name, age);
 
-		scan.close();
+		var scanner = new Scanner(System.in);
+		/*
+		
+		System.out.printf("Quanto é 2 + 2 ?\n");
+		var result = scanner.nextInt();
+		var isRight = result == 4; Operador de igualdade
+		System.out.printf("O resultado é 4, você acertou? (%s)", isRight);
+		var isWrong = result != 4; //Operador de desigualdade
+		System.out.printf("O resultado é 4, você errou? (%s)\n", isWrong);
+		System.out.printf("O resultado é 4, você acertou? (%s)\n", !isWrong); //Operador de inversão
+		
+		*/
+		
+		System.out.println("Quantos anos você tem? ");
+		var age = scanner.nextInt();
+		System.out.println("Você é emancipado? ");
+		var isEmancipated = scanner.nextBoolean();
+		//var canDrive = age > 17; //Operador de comparação "Maior". Mesmo para "Menor"
+		var canDrive = age >= 18 || isEmancipated; //Operador de comparação "Maior ou igual". Mesmo para "Menor ou igual"
+		System.out.printf("Você pode dirigir? (%s)", canDrive);
+		
+		scanner.close();
 		
 	}
 
