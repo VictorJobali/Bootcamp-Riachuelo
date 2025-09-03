ff4600aea42a9d9af0afd0b377c79adecbfc7fcc
João Victor Batista de Oliveira <jvboliveira0405@gmail.com>
2025-09-03 18:20:08 -0300

    Estrutura de condicional switch case

--- get-started/src/Main.java
+++ get-started/src/Main.java
@@ -6,20 +6,75 @@ public class Main {
 
 		var scanner = new Scanner(System.in);
 		
-		System.out.println("informe seu nome: ");
-		var name = scanner.next();
-		System.out.println("informe sua idade: ");
-		var age = scanner.nextInt();		
-		System.out.println("Você é emancipado? (s/n)");
-		var isEmancipated = scanner.next().equalsIgnoreCase("s");
+		/*
+		  
+		 SWITCH CASE COM "INT" 
+		  
+		  
+		System.out.println("Informe um número de 1 até 7:");
+		var option = scanner.nextInt();
+		switch(option) {
+			case 1:
+			case 7:
+				System.out.println("Fim de semana!!");
+				break;
+			case 2:
+				System.out.println("Segunda");
+				break;
+			case 3:
+				System.out.println("Terça");
+				break;
+			case 4:
+				System.out.println("Quarta");
+				break;
+			case 5:
+				System.out.println("Quinta");
+				break;
+			case 6:
+				System.out.println("Sexta");
+			default:
+				System.out.println("Opção inválida");
+		}
+		   
+		   */
 		
-		var canDrive = (age >= 18) || (age >= 16 && isEmancipated);
-		var message = canDrive ? 
-				name + ", pode dirigir \n" :
-				name + ", você não pode dirigir \n";
+		
+		  
+/*		 
+  		SWITCH CASE sem "break" 
+		  
+		  
+		System.out.println("Informe um número de 1 até 7:");
+		var option = scanner.nextInt();
+		switch(option) {
+			case 1, 7 -> System.out.println("Fim de semana");
+			case 2 -> System.out.println("Segunda");
+			case 3 -> System.out.println("Terça");
+			case 4 -> System.out.println("Quarta");
+			case 5 -> System.out.println("Quinta");
+			case 6 -> System.out.println("Sexta");
+			default -> System.out.println("Opção inválida");
+		
+		}*/
+		   
+		   
+		System.out.println("Informe um número de 1 até 7:");
+		var option = scanner.nextInt();
+		var message = switch(option) {
+			case 1, 7 -> {
+				var day = option == 1 ? "Domingo" : "Sábado";
+				yield String.format("Hoje é %s, fim de semana !!!", day);
+			}
+			case 2 -> "Segunda";
+			case 3 -> "Terça";
+			case 4 -> "Quarta";
+			case 5 -> "Quinta";
+			case 6 -> "Sexta";
+			default -> "Opção inválida";
+		
+		};
 		
 		System.out.println(message);
-		System.out.println("Fim da execução");
 		
 		scanner.close();
 		
