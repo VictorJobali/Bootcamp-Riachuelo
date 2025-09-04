547c48deefd745a1491e91bb013bdcc9f5002889
João Victor Batista de Oliveira <jvboliveira0405@gmail.com>
2025-09-04 16:16:02 -0300

    Estrutura de repetição 'while' e 'do...while'

--- get-started/src/Main.java
+++ get-started/src/Main.java
@@ -1,10 +1,67 @@
+import java.util.Scanner;
+
 public class Main {
 	
 	public static void main(String[] args) {	
-		for(var i = 0; i < args.length; i++) {
-			System.out.println(args[i]);
-			
+
+		var scanner = new Scanner(System.in);
+		
+		/*
+		   =======================================
+			Exemplo utilizando operador de negação
+		   =======================================
+		
+		var name = "";  
+		while (!name.equalsIgnoreCase("exit")) {
+			System.out.println("Informe um nome:");
+			name = scanner.next();
+			System.out.println(name);
 		}
+		
+		System.out.println("Você informou o nome correto!");
+		
+		*/
+		
+		/*
+		  =====================================
+		  Exemplo utilizando a keyword 'break'
+		  =====================================
+		  
+		var name = "";  
+		while (true) {
+			System.out.println("Informe um nome:");
+			name = scanner.next();
+			System.out.println(name);
+			
+			if(name.equalsIgnoreCase("exit")) break;
+		
+		}
+		
+		System.out.println("Você informou o nome correto!");
+		
+		*/
+		
+		/*
+		 =========================== 
+		 Exemplo usando 'do...while' 
+		 =========================== 
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		*/
+		var name = "exit";
+		do {
+			System.out.println("Informe um nome:");
+			name = scanner.next();
+			System.out.println(name);
+		
+		} while (!name.equalsIgnoreCase("exit"));
+		
+		scanner.close();
 		
 	}
 
