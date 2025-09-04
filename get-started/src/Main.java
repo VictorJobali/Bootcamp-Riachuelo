cef8f43da644b01dd6fc070d7b17a3d412c0a37b
João Victor Batista de Oliveira <jvboliveira0405@gmail.com>
2025-09-04 18:27:51 -0300

    Exercício: Par ou impar em um intervalo informado

--- get-started/src/Main.java
+++ get-started/src/Main.java
@@ -6,24 +6,29 @@ public class Main {
 
 		var scanner = new Scanner(System.in);
 
-		System.out.print("Informe sua altura(cm):");
-		var height = scanner.nextFloat();
-		System.out.print("Informe seu peso(kg):");
-		var weight = scanner.nextFloat();
-		var BMI = (weight/(Math.pow(height, 2)));
-
-		if(BMI <= 18.5) {
-			System.out.println("Abaixo do peso");
-		} else if (BMI >= 18.6 && BMI <= 24.9 ) {
-			System.out.println("Peso ideal");
-		} else if (BMI >= 25.0 && BMI <= 29.9 ) {
-			System.out.println("Levemente acima do peso");
-		} else if (BMI >= 30.0 && BMI <= 34.9 ) {
-			System.out.println("Obesidade grau I");
-		} else if (BMI >= 35.0 && BMI <= 39.9 ) {
-			System.out.println("Obesidade grau II (Severa)");
+		System.out.print("Informe o primeiro número:");
+		var n1 = scanner.nextInt();
+		var n2 = 0;
+		while(true) {
+			System.out.print("Informe o segundo número:");
+			n2 = scanner.nextInt();
+			if(n2 > n1) break;
+			System.out.printf("Informe um número maior que %s\n",n1);
+		}
+		System.out.print("Você quer par ou ímpar?");
+		var choice = scanner.next();
+		if (choice.equalsIgnoreCase("par")) {
+			for (var i = n1; i <= n2 ; i++) {
+				if (i % 2 == 0) {
+					System.out.println(i);
+				}
+			}
 		} else {
-			System.out.println("Obesidade III (Mórbida)");
+			for (var i = n1; i <= n2; i++) {
+				if(i % 2 == 1) {
+					System.out.println(i);
+				}
+			}
 		}
 		
 		scanner.close();
